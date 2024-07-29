176f19ad36cbda835937952521dfa7f57a7e330f
Dmitry Aliskerov <dmitry.aliskerov@gmail.com>
2024-07-29 13:47:52 +0400

    config update

--- Collector.Worker/config/prod.exs
+++ Collector.Worker/config/prod.exs
@@ -1,7 +1,8 @@
 import Config
 
-# Configure your database to ElephantSQL
 config :worker, Collector.Repo,
+#  url: "postgres://qojtvwmd:kz-gzgywFRYa0M7vlumg6CXm3iHsZuSR@john.db.elephantsql.com/qojtvwmd",
+#  maintenance_database: "qojtvwmd",
   url: "postgresql://collector:gqhbkDLKlBP2RRzigxqk6kOyTS14oSOl@dpg-cqj1ob0gph6c738vrsjg-a.singapore-postgres.render.com/collector_7q0n",
   maintenance_database: "collector_7q0n",
   ssl: true,
@@ -17,3 +18,31 @@ config :worker, Collector.Repo,
   queue_target: 5000,
   pool_size: 10,
   max_connections: 1
+
+config :worker, WorkerWeb.Endpoint,
+  http: [ip: {0, 0, 0, 0}, port: 80],
+  check_origin: false,
+  code_reloader: true,
+  debug_errors: true,
+  secret_key_base: "C9JUJdHidxat8DdYq6SPRHt9WYdyKVhAYXVntKoPg9KhayMHkvXnjQICG22gNvmC",
+  watchers: []
+
+config :worker, WorkerWeb.Endpoint,
+  live_reload: [
+    patterns: [
+      ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
+      ~r"lib/worker_web/(live|views)/.*(ex)$",
+      ~r"lib/worker_web/templates/.*(eex)$"
+    ]
+  ]
+
+config :logger, :console, format: "[$level] $message\n"
+
+config :phoenix, :stacktrace_depth, 20
+
+config :phoenix, :plug_init_mode, :runtime
+
+config :phoenix, :json_library, Jason
+
+config :worker, WorkerWeb.Endpoint,
+  pubsub_server: Collector.PubSub
